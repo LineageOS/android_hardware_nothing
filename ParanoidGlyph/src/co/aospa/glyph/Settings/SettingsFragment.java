e6224c71fcf0f406b85886bdc7b5e41281a87833
Bruno Martins <bgcngm@gmail.com>
2025-11-25 20:56:05 +0000

    ParanoidGlyph: Use new method to listen for preference changes
    Usage of addOnSwitchChangeListener(OnCheckedChangeListener) is
    deprecated. The Javadoc explicitly says:
    
      Use setOnPreferenceChangeListener(OnPreferenceChangeListener) instead.
    
    Change-Id: Ice360a37e226d703050eabc8db0b63c49a79c24f
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- ParanoidGlyph/src/co/aospa/glyph/Settings/SettingsFragment.java
+++ ParanoidGlyph/src/co/aospa/glyph/Settings/SettingsFragment.java
@@ -24,8 +24,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.Settings;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
 
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
@@ -41,10 +39,7 @@ import co.aospa.glyph.Constants.Constants;
 import co.aospa.glyph.Manager.SettingsManager;
 import co.aospa.glyph.Utils.ServiceUtils;
 
-public class SettingsFragment extends PreferenceFragment implements OnPreferenceChangeListener,
-        OnCheckedChangeListener {
-
-    private MainSwitchPreference mSwitchBar;
+public class SettingsFragment extends PreferenceFragment implements OnPreferenceChangeListener {
 
     private SwitchPreferenceCompat mFlipPreference;
     private PrimarySwitchPreference mNotifsPreference;
@@ -69,9 +64,9 @@ public class SettingsFragment extends PreferenceFragment implements OnPreference
 
         boolean glyphEnabled = SettingsManager.isGlyphEnabled();
 
-        mSwitchBar = (MainSwitchPreference) findPreference(Constants.GLYPH_ENABLE);
-        mSwitchBar.addOnSwitchChangeListener(this);
-        mSwitchBar.setChecked(glyphEnabled);
+        MainSwitchPreference switchBar = findPreference(Constants.GLYPH_ENABLE);
+        switchBar.setOnPreferenceChangeListener(this);
+        switchBar.setChecked(glyphEnabled);
 
         mFlipPreference = (SwitchPreferenceCompat) findPreference(Constants.GLYPH_FLIP_ENABLE);
         mFlipPreference.setOnPreferenceChangeListener(this);
@@ -121,6 +116,20 @@ public class SettingsFragment extends PreferenceFragment implements OnPreference
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         final String preferenceKey = preference.getKey();
 
+        if (preferenceKey.equals(Constants.GLYPH_ENABLE)) {
+            boolean isChecked = (Boolean) newValue;
+            SettingsManager.enableGlyph(isChecked);
+
+            mFlipPreference.setEnabled(isChecked && !mMusicVisualizerPreference.isChecked());
+            mNotifsPreference.setEnabled(isChecked && !mMusicVisualizerPreference.isChecked());
+            mNotifsPreference.setSwitchEnabled(isChecked && !mMusicVisualizerPreference.isChecked());
+            mCallPreference.setEnabled(isChecked && !mMusicVisualizerPreference.isChecked());
+            mCallPreference.setSwitchEnabled(isChecked && !mMusicVisualizerPreference.isChecked());
+            mChargingLevelPreference.setEnabled(isChecked && !mMusicVisualizerPreference.isChecked());
+            mChargingPowersharePreference.setEnabled(isChecked && !mMusicVisualizerPreference.isChecked());
+            mVolumeLevelPreference.setEnabled(isChecked && !mMusicVisualizerPreference.isChecked());
+        }
+
         if (preferenceKey.equals(Constants.GLYPH_CALL_ENABLE)) {
             SettingsManager.setGlyphCallEnabled(!mCallPreference.isChecked());
         }
@@ -145,24 +154,6 @@ public class SettingsFragment extends PreferenceFragment implements OnPreference
         mHandler.post(() -> ServiceUtils.checkGlyphService());
 
         return true;
-    }
-
-    @Override
-    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        SettingsManager.enableGlyph(isChecked);
-
-        mSwitchBar.setChecked(isChecked);
-
-        mFlipPreference.setEnabled(isChecked && !mMusicVisualizerPreference.isChecked());
-        mNotifsPreference.setEnabled(isChecked && !mMusicVisualizerPreference.isChecked());
-        mNotifsPreference.setSwitchEnabled(isChecked && !mMusicVisualizerPreference.isChecked());
-        mCallPreference.setEnabled(isChecked && !mMusicVisualizerPreference.isChecked());
-        mCallPreference.setSwitchEnabled(isChecked && !mMusicVisualizerPreference.isChecked());
-        mChargingLevelPreference.setEnabled(isChecked && !mMusicVisualizerPreference.isChecked());
-        mChargingPowersharePreference.setEnabled(isChecked && !mMusicVisualizerPreference.isChecked());
-        mVolumeLevelPreference.setEnabled(isChecked && !mMusicVisualizerPreference.isChecked());
-
-        mHandler.post(() -> ServiceUtils.checkGlyphService());
     }
 
     @Override
