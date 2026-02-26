fa57988971a609b5a25fca4cda215ab529c13298
Saalim Quadri <danascape@gmail.com>
2026-02-26 18:15:34 +0530

    nothing: Initial sepolicy Makefile
    Change-Id: Ib183efae34143bf763716dad3acb0a39657201ea
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

---
+++ sepolicy/nt-sepolicy.mk
@@ -0,0 +1,10 @@
+#
+# SPDX-FileCopyrightText: 2026 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += hardware/nothing/sepolicy/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += hardware/nothing/sepolicy/private
+
+BOARD_VENDOR_SEPOLICY_DIRS += hardware/nothing/sepolicy/vendor
+TEST
