0785e4ada9138a96f58958903dcd22109242d758
Saalim Quadri <danascape@gmail.com>
2026-02-27 03:37:49 +0530

    ParanoidGlyph: Rename packages
    - Also move Constants to utils/
    
    Change-Id: Iafc87c463186ee9ddfc6c5a9afb15097772cafce
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

---
+++ packages/ParanoidGlyph/src/co/aospa/glyph/settings/NotifsSettingsFragment.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright (C) 2022-2024 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package co.aospa.glyph.settings;
+
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+
+import androidx.preference.ListPreference;
+import androidx.preference.MultiSelectListPreference;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.SwitchPreference;
+
+import com.android.internal.util.ArrayUtils;
+import com.android.settingslib.widget.MainSwitchPreference;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import co.aospa.glyph.R;
+import co.aospa.glyph.utils.Constants;
+import co.aospa.glyph.manager.SettingsManager;
+import co.aospa.glyph.preference.GlyphAnimationPreference;
+import co.aospa.glyph.utils.ResourceUtils;
+import co.aospa.glyph.utils.ServiceUtils;
+
+public class NotifsSettingsFragment extends PreferenceFragment implements OnPreferenceChangeListener,
+        OnCheckedChangeListener {
+
+    private PreferenceScreen mScreen;
+
+    private MainSwitchPreference mSwitchBar;
+    private PreferenceCategory mCategory;
+
+    private List<String> mEssentialApps = new ArrayList<String>();
+    private List<String> mEssentialAppsNames = new ArrayList<String>();
+
+    private PackageManager mPackageManager;
+
+    private ListPreference mListPreference;
+    private MultiSelectListPreference mMultiSelectListPreference;
+
+    private GlyphAnimationPreference mGlyphAnimationPreference;
+
+    private Handler mHandler = new Handler();
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.glyph_notifs_settings);
+
+        mScreen = this.getPreferenceScreen();
+        getActivity().setTitle(R.string.glyph_settings_notifs_toggle_title);
+
+        mSwitchBar = (MainSwitchPreference) findPreference(Constants.GLYPH_NOTIFS_SUB_ENABLE);
+        mSwitchBar.addOnSwitchChangeListener(this);
+        mSwitchBar.setChecked(SettingsManager.isGlyphNotifsEnabled());
+
+        mCategory = (PreferenceCategory) findPreference(Constants.GLYPH_NOTIFS_SUB_CATEGORY);
+
+        mListPreference = (ListPreference) findPreference(Constants.GLYPH_NOTIFS_SUB_ANIMATIONS);
+        mListPreference.setOnPreferenceChangeListener(this);
+        mListPreference.setEntries(ResourceUtils.getNotificationAnimations());
+        mListPreference.setEntryValues(ResourceUtils.getNotificationAnimations());
+        if (!ArrayUtils.contains(ResourceUtils.getNotificationAnimations(), mListPreference.getValue())) {
+            mListPreference.setValue(ResourceUtils.getString("glyph_settings_notifs_animations_default"));
+        }
+
+        mGlyphAnimationPreference = (GlyphAnimationPreference) findPreference(Constants.GLYPH_NOTIFS_SUB_PREVIEW);
+
+        mPackageManager = getActivity().getPackageManager();
+        List<ApplicationInfo> mApps = mPackageManager.getInstalledApplications(PackageManager.GET_GIDS);
+        Collections.sort(mApps, new ApplicationInfo.DisplayNameComparator(mPackageManager));
+        for (ApplicationInfo app : mApps) {
+            if(mPackageManager.getLaunchIntentForPackage(app.packageName) != null  && !ArrayUtils.contains(Constants.APPS_TO_IGNORE, app.packageName)) { // apps with launcher intent
+                SwitchPreference mSwitchPreference = new SwitchPreference(mScreen.getContext());
+                mSwitchPreference.setKey(app.packageName);
+                mSwitchPreference.setTitle(" " + app.loadLabel(mPackageManager).toString()); // add this space since the layout looks off otherwise
+                mSwitchPreference.setIcon(app.loadIcon(mPackageManager));
+                mSwitchPreference.setDefaultValue(true);
+                mSwitchPreference.setOnPreferenceChangeListener(this);
+                mCategory.addPreference(mSwitchPreference);
+
+                mEssentialApps.add(app.packageName);
+                mEssentialAppsNames.add(app.loadLabel(mPackageManager).toString());
+            }
+        }
+
+        mMultiSelectListPreference = (MultiSelectListPreference) findPreference(Constants.GLYPH_NOTIFS_SUB_ESSENTIAL);
+        mMultiSelectListPreference.setOnPreferenceChangeListener(this);
+        mMultiSelectListPreference.setEntries(mEssentialAppsNames.toArray(new CharSequence[0]));
+        mMultiSelectListPreference.setEntryValues(mEssentialApps.toArray(new CharSequence[0]));
+
+    }
+
+    @Override
+    public void onViewCreated (View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mGlyphAnimationPreference.updateAnimation(SettingsManager.isGlyphNotifsEnabled(),
+                SettingsManager.getGlyphNotifsAnimation(), 1500);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        final String preferenceKey = preference.getKey();
+
+        if (preferenceKey.equals(Constants.GLYPH_NOTIFS_SUB_ANIMATIONS)) {
+            mGlyphAnimationPreference.updateAnimation(SettingsManager.isGlyphNotifsEnabled(),
+                newValue.toString(), 1500);
+        }
+
+        if (preferenceKey.equals(Constants.GLYPH_NOTIFS_SUB_ESSENTIAL)) {
+            //if (DEBUG) Log.d(TAG, "onPreferenceChange: " + newValue.toString());
+        }
+
+        //mHandler.post(() -> ServiceUtils.checkGlyphService());
+
+        return true;
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        SettingsManager.setGlyphNotifsEnabled(isChecked);
+        ServiceUtils.checkGlyphService();
+        mGlyphAnimationPreference.updateAnimation(isChecked,
+                SettingsManager.getGlyphNotifsAnimation(), 1500);
+    }
+
+}
---
+++ packages/ParanoidGlyph/src/co/aospa/glyph/utils/ServiceUtils.java
@@ -0,0 +1,171 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *               2017-2019 The LineageOS Project
+ *               2020-2024 Paranoid Android
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package co.aospa.glyph.utils;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.UserHandle;
+import android.util.Log;
+
+import co.aospa.glyph.utils.Constants;
+import co.aospa.glyph.manager.SettingsManager;
+import co.aospa.glyph.services.CallReceiverService;
+import co.aospa.glyph.services.ChargingService;
+import co.aospa.glyph.services.FlipToGlyphService;
+import co.aospa.glyph.services.MusicVisualizerService;
+import co.aospa.glyph.services.PowershareService;
+import co.aospa.glyph.services.ThirdPartyService;
+import co.aospa.glyph.services.VolumeLevelService;
+
+public final class ServiceUtils {
+
+    private static final String TAG = "GlyphServiceUtils";
+    private static final boolean DEBUG = true;
+
+    private static Context context = Constants.CONTEXT;
+
+    private static void startCallReceiverService() {
+        if (DEBUG) Log.d(TAG, "Starting Glyph call receiver service");
+        context.startServiceAsUser(new Intent(context, CallReceiverService.class),
+                UserHandle.CURRENT);
+    }
+
+    private static void stopCallReceiverService() {
+        if (DEBUG) Log.d(TAG, "Stopping Glyph call receiver service");
+        context.stopServiceAsUser(new Intent(context, CallReceiverService.class),
+                UserHandle.CURRENT);
+    }
+
+    private static void startChargingService() {
+        if (DEBUG) Log.d(TAG, "Starting Glyph charging service");
+        context.startServiceAsUser(new Intent(context, ChargingService.class),
+                UserHandle.CURRENT);
+    }
+
+    private static void stopChargingService() {
+        if (DEBUG) Log.d(TAG, "Stopping Glyph charging service");
+        context.stopServiceAsUser(new Intent(context, ChargingService.class),
+                UserHandle.CURRENT);
+    }
+
+    private static void startFlipToGlyphService() {
+        if (DEBUG) Log.d(TAG, "Starting Flip to Glyph service");
+        context.startServiceAsUser(new Intent(context, FlipToGlyphService.class),
+                UserHandle.CURRENT);
+    }
+
+    private static void stopFlipToGlyphService() {
+        if (DEBUG) Log.d(TAG, "Stopping Flip to Glyph service");
+        context.stopServiceAsUser(new Intent(context, FlipToGlyphService.class),
+                UserHandle.CURRENT);
+    }
+
+    public static void startMusicVisualizerService() {
+        if (DEBUG) Log.d(TAG, "Starting Music Visualizer service");
+        context.startServiceAsUser(new Intent(context, MusicVisualizerService.class),
+                UserHandle.CURRENT);
+    }
+
+    protected static void stopMusicVisualizerService() {
+        if (DEBUG) Log.d(TAG, "Stopping Music Visualizer service");
+        context.stopServiceAsUser(new Intent(context, MusicVisualizerService.class),
+                UserHandle.CURRENT);
+    }
+
+    private static void startPowershareService() {
+        if (DEBUG) Log.d(TAG, "Starting Glyph powershare service");
+        context.startServiceAsUser(new Intent(context, PowershareService.class),
+                UserHandle.CURRENT);
+    }
+
+    private static void stopPowershareService() {
+        if (DEBUG) Log.d(TAG, "Stopping Glyph powershare service");
+        context.stopServiceAsUser(new Intent(context, PowershareService.class),
+                UserHandle.CURRENT);
+    }
+
+    public static void startVolumeLevelService() {
+        if (DEBUG) Log.d(TAG, "Starting Volume Level service");
+        context.startServiceAsUser(new Intent(context, VolumeLevelService.class),
+                UserHandle.CURRENT);
+    }
+
+    protected static void stopVolumeLevelService() {
+        if (DEBUG) Log.d(TAG, "Stopping Volume Listener service");
+        context.stopServiceAsUser(new Intent(context, VolumeLevelService.class),
+                UserHandle.CURRENT);
+    }
+
+    public static void startThirdPartyService() {
+        if (DEBUG) Log.d(TAG, "Starting ThirdParty service");
+        context.startServiceAsUser(new Intent(context, ThirdPartyService.class),
+                UserHandle.CURRENT);
+    }
+
+    protected static void stopThirdPartyService() {
+        if (DEBUG) Log.d(TAG, "Stopping ThirdParty service");
+        context.stopServiceAsUser(new Intent(context, ThirdPartyService.class),
+                UserHandle.CURRENT);
+    }
+
+    public static void checkGlyphService() {
+        if (SettingsManager.isGlyphEnabled()) {
+            Constants.setBrightness(SettingsManager.getGlyphBrightness());
+            startThirdPartyService();
+            if (SettingsManager.isGlyphChargingEnabled()) {
+                startChargingService();
+            } else {
+                stopChargingService();
+            }
+            if (SettingsManager.isGlyphPowershareEnabled()) {
+                startPowershareService();
+            } else {
+                stopPowershareService();
+            }
+            if (SettingsManager.isGlyphCallEnabled()) {
+                startCallReceiverService();
+            } else {
+                stopCallReceiverService();
+            }
+            if (SettingsManager.isGlyphFlipEnabled()) {
+                startFlipToGlyphService();
+            } else {
+                stopFlipToGlyphService();
+            }
+            if (SettingsManager.isGlyphMusicVisualizerEnabled()) {
+                startMusicVisualizerService();
+            } else {
+                stopMusicVisualizerService();
+            }
+            if (SettingsManager.isGlyphVolumeLevelEnabled()) {
+                startVolumeLevelService();
+            } else {
+                stopVolumeLevelService();
+            }
+        } else {
+            stopChargingService();
+            stopPowershareService();
+            stopCallReceiverService();
+            stopFlipToGlyphService();
+            stopMusicVisualizerService();
+            stopVolumeLevelService();
+            stopThirdPartyService();
+        }
+    }
+}
