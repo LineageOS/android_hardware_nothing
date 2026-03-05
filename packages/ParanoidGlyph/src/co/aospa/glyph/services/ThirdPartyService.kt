2a68d127036e46c459bb6fba5911871951a095c3
Saalim Quadri <danascape@gmail.com>
2026-03-06 03:06:27 +0530

    ParanoidGlyph: Sync TorchTileService state with external toggles
    Change-Id: I042d61500f76e50603072fb0b5971837591beac1
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- packages/ParanoidGlyph/src/co/aospa/glyph/services/ThirdPartyService.kt
+++ packages/ParanoidGlyph/src/co/aospa/glyph/services/ThirdPartyService.kt
@@ -10,6 +10,11 @@ import android.content.Intent
 import android.os.IBinder
 import android.util.Log
 import co.aospa.glyph.manager.AnimationManager
+import co.aospa.glyph.manager.SettingsManager
+import co.aospa.glyph.manager.StatusManager
+import co.aospa.glyph.tiles.TorchTileService
+import co.aospa.glyph.utils.FileUtils
+import co.aospa.glyph.utils.ResourceUtils
 import com.nothing.thirdparty.IGlyphService
 
 class ThirdPartyService : Service() {
@@ -19,6 +24,22 @@ class ThirdPartyService : Service() {
             override fun setFrameColors(iArray: IntArray?) {
                 Log.d("ThirdPartyService", "received data: ${iArray.contentToString()}")
                 AnimationManager.updateLedFrame(iArray)
+            }
+
+            override fun setGlyphTorch(active: Boolean) {
+                Log.d("ThirdPartyService", "setGlyphTorch: $active")
+                val brightness = SettingsManager.getGlyphBrightness()
+                StatusManager.setAllLedsActive(active)
+                FileUtils.writeAllLed(if (active) brightness else 0)
+                if (StatusManager.isEssentialLedActive() && !active) {
+                    FileUtils.writeSingleLed(
+                        ResourceUtils.getInteger("glyph_settings_notifs_essential_led"),
+                        (brightness / 100 * 7).toFloat(),
+                    )
+                }
+                sendBroadcast(
+                    Intent(TorchTileService.ACTION_TORCH_STATE_CHANGED).setPackage(packageName)
+                )
             }
 
             override fun openSession() {
